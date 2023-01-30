9a3c7a3898fd1fd22b38e27e91c751b73bc747e5
Xiaokang Fu <1072534112@qq.com>
2023-01-30 21:34:08 +0000

    add more doc for bing router

--- georouting/routers/bing.py
+++ georouting/routers/bing.py
@@ -2,47 +2,129 @@ import requests
 import pandas as pd
 from georouting.routers.base import WebRouter, Route, BingRoute
 import json
+
 # from georouting.routers.base import BaseRouter
 
+# smilarly to google.py, we need to improve the documentation
 class BingRouter(WebRouter):
-    """Bing Maps API router"""
+    """
+    Bing Maps API router.
+    The BingRouter class is a subclass of the WebRouter class and is used for routing using the Bing Maps API.
+    This class is designed to provide a convenient and easy-to-use interface for interacting with the Bing Maps API.
 
-    def __init__(self, api_key,mode="driving", timeout=10, language="en"):
+    Parameters
+    ----------
+
+    api_key : str
+        The API key for the Bing Maps API.
+
+    mode : str
+        The routing mode. Can be either "driving" or "walking".
+
+    timeout : int
+        The timeout in seconds for API requests.
+
+    language : str
+        The language to be used in API requests.
+
+    Attributes
+    ----------
+    api_key : str
+        The API key for the Bing Maps API.
+
+    mode : str
+        The routing mode. Can be either "driving" or "walking".
+
+    timeout : int
+        The timeout in seconds for API requests.
+
+    language : str
+        The language to be used in API requests.
+
+    base_url : str
+        The base URL for the Bing Maps API.
+
+    """
+
+    def __init__(self, api_key, mode="driving", timeout=10, language="en"):
         super().__init__(api_key, mode=mode, timeout=timeout, language=language)
         self.base_url = "https://dev.virtualearth.net/REST/v1/Routes/"
 
     def _get_directions_url(self, origin, destination):
+        """
+        Helper function for getting the URL for a directions request.
+        """
 
-        return "%s%s?wp.0=%f,%f&wp.1=%f,%f&routeAttributes=routePath&key=%s" % (self.base_url,self.mode,
-            origin[0], origin[1], destination[0], destination[1], self.api_key)
+        return "%s%s?wp.0=%f,%f&wp.1=%f,%f&routeAttributes=routePath&key=%s" % (
+            self.base_url,
+            self.mode,
+            origin[0],
+            origin[1],
+            destination[0],
+            destination[1],
+            self.api_key,
+        )
 
     def _get_matrix_distance_url(self, origins, destinations):
-        origins = [str(item[0]) + "," + str(item[1])  for item in origins]
-        destinations = [str(item[0]) + "," + str(item[1])  for item in destinations]
+        """
+        Helper function for getting the URL for a distance matrix request.
+        """
+
+        origins = [str(item[0]) + "," + str(item[1]) for item in origins]
+        destinations = [str(item[0]) + "," + str(item[1]) for item in destinations]
         origins = ";".join(origins)
         destinations = ";".join(destinations)
-        return "%sDistanceMatrix?origins=%s&destinations=%s&travelMode=%s&timeUnit=second&key=%s" % (self.base_url,
-            origins,destinations, self.mode, self.api_key)
+        return (
+            "%sDistanceMatrix?origins=%s&destinations=%s&travelMode=%s&timeUnit=second&key=%s"
+            % (self.base_url, origins, destinations, self.mode, self.api_key)
+        )
 
     def _parse_distance_matrix(self, json_data):
+        """
+        Helper function for parsing the response from a distance matrix request.
+        """
 
-        df = pd.DataFrame(json_data['resourceSets'][0]['resources'][0]['results'])
-        df = df[['travelDistance','travelDuration']]
-        df.columns = ['distance (m)','duration (s)']
+        df = pd.DataFrame(json_data["resourceSets"][0]["resources"][0]["results"])
+        df = df[["travelDistance", "travelDuration"]]
+        df.columns = ["distance (m)", "duration (s)"]
         return df
 
     def get_route(self, origin, destination):
+        """
+        Get a route between two points.
+        Parameters
+        ----------
+        origin : tuple
+            The origin point.
+        destination : tuple
+            The destination point.
+        Returns
+        -------
+        route : Route
+            The route between the origin and destination.
+        """
 
         url = self._get_directions_url(origin, destination)
         route = super()._get_request(url)
         route = Route(BingRoute(route))
         return route
-    
-    def get_distance_matrix(self, origins, destinations,append_od=False):
 
-        # FIXME: the url seems to be different from google and bing 
-        # and it enable to get part of the matrix, that's awesome, acutally 
-        # but can I do the simillar thing for google and bing? 
+    def get_distance_matrix(self, origins, destinations, append_od=False):
+
+        """
+        Get a distance matrix between a list of origins and destinations.
+        Parameters
+        ----------
+        origins : list
+            A list of origin points.
+        destinations : list
+            A list of destination points.
+        Returns
+        -------
+        distance_matrix : pandas.DataFrame
+            A pandas DataFrame containing the distance matrix.
+        """
+
         url = self._get_matrix_distance_url(origins, destinations)
         res = super()._get_request(url)
         distance_matrix = self._parse_distance_matrix(res)
@@ -51,4 +133,3 @@ class BingRouter(WebRouter):
             distance_matrix = pd.concat([od_matrix, distance_matrix], axis=1)
 
         return distance_matrix
-
--- georouting/routers/osrm.py
+++ georouting/routers/osrm.py
@@ -7,54 +7,81 @@ from georouting.routers.base import WebRouter, Route, OSRMRoute
 # add document for this class
 class OSRMRouter(WebRouter):
     """
-    OSRM router. 
+    OSRM router.
     The base_url is the url of the osrm server,
     the default is the public server of osrm,
     you can also use your own server.
     """
 
-    def __init__(self,mode="driving", timeout=10, language="en",base_url="http://router.project-osrm.org"):
-        super().__init__(api_key=None, mode=mode, timeout=timeout, language=language,base_url=None)
+    def __init__(
+        self,
+        mode="driving",
+        timeout=10,
+        language="en",
+        base_url="http://router.project-osrm.org",
+    ):
+        super().__init__(
+            api_key=None, mode=mode, timeout=timeout, language=language, base_url=None
+        )
         # nned let user reset the base_url
         self.base_url = base_url
 
-
     def _get_directions_url(self, origin, destination):
         """
-        Helper 
+        Helper
         """
-        return "%s/route/v1/%s/%f,%f;%f,%f?steps=true&annotations=true&geometries=geojson" % (
-            self.base_url, self.mode, origin[1], origin[0], destination[1], destination[0])
-
+        return (
+            "%s/route/v1/%s/%f,%f;%f,%f?steps=true&annotations=true&geometries=geojson"
+            % (
+                self.base_url,
+                self.mode,
+                origin[1],
+                origin[0],
+                destination[1],
+                destination[0],
+            )
+        )
 
     def _get_matrix_distance_url(self, origins, destinations):
 
-# get the need cal location
-        s = str(list(range(len(origins)))).replace(",",";").replace("[","").replace("]","").replace(" ","")
-        d = str(list(range(
-            len(origins),len(origins)+len(destinations)
-        ))).replace(",",";").replace("[","").replace("]","").replace(" ","")
+        # get the need cal location
+        s = (
+            str(list(range(len(origins))))
+            .replace(",", ";")
+            .replace("[", "")
+            .replace("]", "")
+            .replace(" ", "")
+        )
+        d = (
+            str(list(range(len(origins), len(origins) + len(destinations))))
+            .replace(",", ";")
+            .replace("[", "")
+            .replace("]", "")
+            .replace(" ", "")
+        )
 
-        
-        origins = [str(item[1]) + "," + str(item[0])  for item in origins]
-        destinations = [str(item[1]) + "," + str(item[0])  for item in destinations]
+        origins = [str(item[1]) + "," + str(item[0]) for item in origins]
+        destinations = [str(item[1]) + "," + str(item[0]) for item in destinations]
         origins = ";".join(origins)
         destinations = ";".join(destinations)
-     
-        url = '%s/table/v1/%s/%s;%s?sources=%s&destinations=%s&annotations=duration,distance'%(self.base_url,self.mode,origins,destinations,s,d)
+
+        url = (
+            "%s/table/v1/%s/%s;%s?sources=%s&destinations=%s&annotations=duration,distance"
+            % (self.base_url, self.mode, origins, destinations, s, d)
+        )
         return url
 
     def _parse_distance_matrix(self, json_data):
-        durations = json_data['durations']
-        distances = json_data['distances']
+        durations = json_data["durations"]
+        distances = json_data["distances"]
 
         # flatten the list
         durations = [item for sublist in durations for item in sublist]
         distances = [item for sublist in distances for item in sublist]
 
         # combine the dutation and destinations list to a dataframe
-        print(len(durations),len(distances))
-        df = pd.DataFrame({'distance (m)': durations, 'duration (s)': distances})
+        print(len(durations), len(distances))
+        df = pd.DataFrame({"distance (m)": durations, "duration (s)": distances})
 
         return df
 
@@ -64,7 +91,7 @@ class OSRMRouter(WebRouter):
         route = Route(OSRMRoute(route))
         return route
 
-    def get_distance_matrix(self, origins, destinations,append_od=False):
+    def get_distance_matrix(self, origins, destinations, append_od=False):
 
         url = self._get_matrix_distance_url(origins, destinations)
         res = super()._get_request(url)
@@ -74,5 +101,3 @@ class OSRMRouter(WebRouter):
             distance_matrix = pd.concat([od_matrix, distance_matrix], axis=1)
 
         return distance_matrix
-
-  
