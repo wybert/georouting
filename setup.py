cfe8f901f73f00a7c54903e364d5de969bd16348
Xiaokang Fu <1072534112@qq.com>
2025-11-24 19:16:19 +0800

    fix the requirements

--- setup.py
+++ setup.py
@@ -27,17 +27,18 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Xiaokang Fu",
     author_email='fxk123@gmail.com',
-    python_requires='>=3.7',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
     description="Geo routing for Python users",
     entry_points={
